5edd0e0b175ba5f43233b0756894fe51809333b5
wvit <1999wuwei@gmail.com>
2019-08-22 20:08:17 +0800

    添加部分功能

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -26,8 +26,8 @@ const store = configStore();
 class App extends Component {
   config: Config = {
     pages: [
-      'pages/cloudMusic/cloudMusic', //网易云音乐
       'pages/index/index', //首页
+      'pages/cloudMusic/cloudMusic', //网易云音乐
       'pages/user/user', //用户
       'pages/skill/skill', //技能
       'pages/publishLife/publishLife', //发布生活
